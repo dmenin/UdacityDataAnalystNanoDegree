28d7aecbd222fa1941f767d28d50940c0827f48c
dmenin <dmenin@gmail.com>
2015-07-14 09:26:14 +0100

    R project

--- 4_R/FinalProject/FinalProject.r
+++ 4_R/FinalProject/FinalProject.r
@@ -3,40 +3,73 @@ library(sfsmisc)
 
 setwd("C:/git/UdacityDataAnalystNanoDegree/4_R/FinalProject/")
 df<-read.csv("wineQualityWhites.csv")
-#remove index
-df[,c("X")] <- list(NULL)
+
 df$quality <- as.factor(df$quality)
 head(df)
 
-#histogram
+###########################################histogram
 ggplot(data=df,aes(x=quality))+
   geom_bar(aes(y = (..count..)),fill="orange")+
   geom_text(aes(y = (..count..),label =   ifelse((..count..)==0,"",scales::percent((..count..)/sum(..count..)))), stat="bin",colour="darkgreen")
 
-names(df)
-
-p1<- ggplot(aes(x=quality, y = alcohol), data = df) + geom_boxplot()+  stat_summary(fun.y=mean,geom = 'point', shape = 4)
-p2<- ggplot(aes(x=quality, y = density), data = df) + geom_boxplot()+  stat_summary(fun.y=mean,geom = 'point', shape = 4)
-grid.arrange(p1,p2, ncol=1)
+#names(df)
 
 
-ggplot(aes(x=quality, y = density), data = df) + geom_boxplot()+  stat_summary(fun.y=mean,geom = 'point', shape = 4)
-
-
-
-
-
-
-
+##########################################multiple box plots
+#head(df)
+library(reshape2 )
 library(dplyr)
-select(df, -quality)
+library(ggplot2)
+library(gridExtra)
+library(plyr)
+melt_data <- melt(df,id.vars=c("X","quality"))
+head(melt_data)
 
 
+plots <- dlply(melt_data,.(variable),function(chunk)
+{
+  ggplot(data=chunk, aes(x=factor(quality), y = value)) + geom_boxplot()+  stat_summary(fun.y=mean,geom = 'point', shape = 4) +  labs(y=unique(chunk$variable))
+})
+
+do.call(grid.arrange,c(plots,ncol=3))
+
+
+##########################################multiple GRIDS
+plots <- dlply(melt_data,.(variable),function(chunk)
+{
+  ggplot(data= chunk) +  aes(x = factor(quality), y = value)+
+    stat_summary(aes(fill = factor(quality)), fun.y=mean, geom="bar")+
+    stat_summary(aes(label=round(..y..,4)), fun.y=mean, geom="text", size=6, vjust = 1)  +  
+    labs(y=unique(chunk$variable)) + theme(legend.position="none") +
+    geom_smooth(method = "lm", se=FALSE, color="black", type='dotted', aes(group=1), size =1)
+    
+})
+do.call(grid.arrange,c(plots,ncol=2))
+
+
+
+
+
+
+head(df)
+#remove index
+df[,c("X")] <- list(NULL)
+
+
+
+#corrs?
 cor(select(df, -quality))
 pairs(select(df, -quality))
 
+cor(df$free.sulfur.dioxide, df$total.sulfur.dioxide)
 
 
+
+
+ggplot(data= df) +  aes(x = factor(quality), y = fixed.acidity)+
+  stat_summary(aes(fill = factor(quality)), fun.y=mean, geom="bar")+
+  stat_summary(aes(label=round(..y..,2)), fun.y=mean, geom="text", size=6, vjust = -0.5)+
+  geom_smooth(method = "lm", se=FALSE, color="black", aes(group=1), size =2)
 
 
 #http://stats.stackexchange.com/questions/119835/correlation-between-a-nominal-iv-and-a-continuous-dv-variable/124618#124618
@@ -63,20 +96,7 @@ for(i in 1:9){
   data[,paste0("outcome_",i)]<-rnorm(500)
 }
 
-library(dplyr)
-library(ggplot2)
-library(gridExtra)
-
-library(reshape2 )
-melt_data <- melt(data,id.vars=c("id","quality"))
-head(melt_data)
 
 
-plots <- dlply(melt_data,.(variable),function(chunk)
-    {
-      ggplot(data=chunk, aes(x=factor(quality), y = value)) + geom_boxplot()+  stat_summary(fun.y=mean,geom = 'point', shape = 4) +  labs(y=unique(chunk$variable)
-    )
-})
 
 
-do.call(grid.arrange,c(plots,ncol=2))
