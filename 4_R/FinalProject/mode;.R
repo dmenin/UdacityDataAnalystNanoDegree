ba6d66d5955c2fd78a772cc5c7032026fb73a99a
Diego Menin <dmenin@gmail.com>
2015-12-01 19:29:08 +0000

    D3.js project2

--- 4_R/FinalProject/mode;.R
+++ 4_R/FinalProject/mode;.R
@@ -1,9 +1,9 @@
 setwd('C:\\git\\UdacityDataAnalystNanoDegree\\6_D3js\\FinalProject2')
 
 
-d <- read.csv("data.csv")
+d <- read.csv("train.csv")
 
-
+factor
 head(d)
 d["PassengerId"] <-  list(NULL)
 d["Name"] <-  list(NULL)
@@ -12,6 +12,10 @@ d["Cabin"] <-  list(NULL)
 d["Fare"] <-  list(NULL)
 
 d$family = d$SibSp + d$Parch
+d$Pclass <- factor(d$Pclass)
+
+d["Age"] <-  list(NULL)
+head(d)
 
 library(rpart)
 library(rpart.plot)
@@ -20,7 +24,7 @@ fit  <- rpart(Survived ~ ., method="class", data=d)
 
 
 
-cols <- ifelse(fit$frame$yval == 0, "red", "green4")
+cols <- ifelse(fit$frame$yval == 1, "red", "green4")
 
 prp(fit,     
     digits=6,
@@ -37,12 +41,28 @@ prp(fit,
     split.border.col="darkgray"
 )  
 
-printcp(fit)
-plotcp(fit)
 
-rsq.rpart(fit)  
-print(fit)	
-summary(fit)	
-plot(fit)	
+newd <-subset (d, !( d$Sex=="female" & (Pclass==2 | Pclass==1)))
 
 
+
+fit2  <- rpart(Survived ~ ., method="class", data=newd)
+
+
+
+cols <- ifelse(fit2$frame$yval == 1, "red", "green4")
+
+prp(fit2,     
+    digits=6,
+    extra=1,             
+    branch=.5,           
+    faclen=0,            
+    shadow.col="gray",   
+    branch.lty=3,        
+    split.cex=1.2,       
+    split.prefix="is ",  
+    split.suffix="?",    
+    col=cols, border.col=cols,
+    split.box.col="lightgray",
+    split.border.col="darkgray"
+)  
